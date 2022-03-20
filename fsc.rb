43f1e9835bd9bd77f81bcd847052ffb65c0981bb
Shinichi Okada <okada.shin@gmail.com>
2022-03-20 10:35:57 +0900

    feat: verion 0.1.1

--- fsc.rb
+++ fsc.rb
@@ -1,8 +1,8 @@
 class Fsc < Formula
     desc "Flowbite-svelte CLI displays component props"
     homepage "https://github.com/shinokada/fsc"
-    url "https://github.com/shinokada/fsc/archive/refs/tags/v0.1.0.tar.gz"
-    sha256 "44c83941c95650891178892b74548e6b0d93555a0b68aede0b183c9dc6f5aeb5"
+    url "https://github.com/shinokada/fsc/archive/refs/tags/v0.1.1.tar.gz"
+    sha256 "0ecf1676860903dfed5fe0ff9ae604d27a5567ffd87c623f6e74f9d9a2e73962"
     license "MIT"
 
     depends_on "jq"
